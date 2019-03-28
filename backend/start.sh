9a957b25a2faf3794b0fd69f4d7a8a3a954b3226
Lohitaksh Parmar <lohitaksh.parmar@airtel.com>
2019-03-28 13:53:39 +0530

    Added cli in start up script

--- backend/start.sh
+++ backend/start.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
-export NOTIFICATION_ENV=development
+
+# Start up script for Notification Backend
+environment=development
+
+while [ "$1" != "" ]; do
+	case $1 in
+		-e | --env )	shift
+						environment=$1
+						;;
+		-h | --help)	echo "Usage: ./start.sh -e <environment>"
+						exit
+						;;
+	esac
+	shift
+done
+
+if [ $# -lt 1 ]; then
+	echo "Environment not given. Defaulting to \`development\`"
+fi
+
+export NOTIFICATION_ENV=$environment
 export FLASK_APP=app.py
 flask run
